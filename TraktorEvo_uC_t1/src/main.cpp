9c14455497d1e9a0b65a907251ac244f38babb14
Ropfel17 <150962817+Ropfel17@users.noreply.github.com>
2024-03-05 23:46:08 +0100

    LCD Screen tested

--- TraktorEvo_uC_t1/src/main.cpp
+++ TraktorEvo_uC_t1/src/main.cpp
@@ -1,18 +1,23 @@
 #include <Arduino.h>
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
 
-// put function declarations here:
-int myFunction(int, int);
+LiquidCrystal_I2C lcd(0x38,16,2);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+    lcd.init();                      // initialize the lcd 
+    // Print a message to the LCD.
+    lcd.backlight();
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+    lcd.setCursor(0,0);
+    lcd.print("Hello, world!");
+    delay(500);
+ 
+    for(int i = 0; i <= 9999999999999999; i++){
+        lcd.setCursor(0,1);
+        lcd.print(i,DEC);
+    }
 }
